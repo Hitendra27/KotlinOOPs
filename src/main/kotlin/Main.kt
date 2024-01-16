89f6cbd69e2e12bdd26e9316e64f7d1e87a40413
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-01-16 23:40:02 +0000

    feat: Implemented Kotlin class and object

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,13 @@
 fun main(args: Array<String>) {
-    
+
+    val mobile = Mobile()
+    mobile.brand = "Iphone"
+    mobile.model = "11 pro"
+    mobile.mrp = 10000f
+    mobile.discount = 1000f
+
+    println("Discounted price is: ${mobile.getActualPrice()}")
+
+    mobile.printDetails()
 
 }
--- src/main/kotlin/Mobile.kt
+++ src/main/kotlin/Mobile.kt
@@ -1,2 +1,20 @@
 class Mobile {
+
+    var brand: String = ""
+    var model: String = ""
+    var mrp: Float = 0f
+    var discount: Float = 0f
+
+    fun getActualPrice(): Float {
+        return mrp - discount
+    }
+
+    fun printDetails() {
+        println("Mobile details:")
+        println("Brand: $brand")
+        println("Model:  $model")
+        println("MRP: £ $mrp")
+        println("Discount: £ $discount")
+    }
+
 }
