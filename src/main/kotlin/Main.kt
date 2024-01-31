cb62fa03db5ed5f43893d4804819aef761f99086
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-01-31 01:27:29 +0000

    feat: Implemented lambda function

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,15 +1,11 @@
 
 fun main() {
-    tempCity("Ankara", 27, 31, 82)
-    tempCity("Tokyo", 32, 36, 10)
-    tempCity("Cape Town", 59, 64, 2)
-    tempCity("Guatemala City", 50, 55, 7)
-
+    val trickFunction = trick
+    trick()
+    trickFunction()
 }
 
-fun tempCity(cityName: String, lowTemp: Int, highTemp: Int, rain: Int) {
-    println("City: $cityName")
-    println("Low temperature: $lowTemp, High temprature: $highTemp")
-    println("Chance of rain: $rain%")
-    println()
+val trick =  {
+    println("No treats!")
 }
+
