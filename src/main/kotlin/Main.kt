3c257eda487906470d45193b4c2cb6d8f56ace2b
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-05-07 19:31:09 +0100

    feat: implemented shoe class OOP's

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,63 +1,68 @@
 fun main() {
 
-    val fiction = FictionBook("1984", "George Orwell", 1949, "Dystopian")
-    val acedemic = AcademicBook("Clean Code", "Robert C. Martin", 2008, "Software Engineering")
+    val runningInfo = ShoeInfo("Rubber", "Red")
+    val formalInfo = ShoeInfo("Leather", "Black")
 
-    val library = Library()
-    library.addBook(fiction)
-    library.addBook(acedemic)
+    val myRunningShoes = RunningShoe("Nike", 42, runningInfo)
+    val myFormalShoes = FormalShoe("Clarks", 43, formalInfo)
 
-    library.showCatelog()
+    val shoes: List<Shoe> = listOf(myRunningShoes, myFormalShoes)
 
+    for (shoe in shoes) {
+        println(shoe.getDetails())
+        if (shoe is Wearable) {
+            println(shoe.wear())
+        }
+        println()
+    }
 }
 
-// Base class
-open class Book(
-    val title: String,
-    val author: String,
-    val year: Int
+// Base class (abstract)
+abstract class Shoe(
+    val brand: String,
+    val size: Int
 ) {
-    open fun geDescription(): String {
-        return "'$title' by $author, published in $year"
+    abstract fun getDetails(): String
+}
+
+// Wearable Interface
+interface Wearable {
+    fun wear(): String
+}
+
+// data class to hold Shoe info
+data class ShoeInfo(
+    val material: String,
+    val color: String
+)
+
+// Subclasses of Shoe
+class RunningShoe(
+    brand: String,
+    size: Int,
+    val info: ShoeInfo
+) : Shoe(brand, size), Wearable {
+    override fun getDetails(): String {
+        return "Running Shoe - $brand, Size $size, Material: ${info.material}, Color: ${info.color}"
+    }
+
+    override fun wear(): String {
+        return "Ready for a run with $brand shoes!"
     }
 }
 
-// Subclass
-class FictionBook(
-    title: String,
-    author: String,
-    year: Int,
-    val genre: String
-) : Book(title, author, year) {
-    override fun geDescription(): String {
-        return super.geDescription() + "- Genre: $genre"
-    }
-}
-
-// Subclass 2
-class AcademicBook(
-    title: String,
-    author: String,
-    year: Int,
-    val subject: String
-) : Book(title, author, year) {
-    override fun geDescription(): String {
-        return super.geDescription() + " - Subject: $subject"
-    }
-}
-
-// class demonstrating encapsulation
-class Library {
-    private val books = mutableListOf<Book>() // private property
-
-    fun addBook(book: Book) {
-        books.add(book)
-        println("Book added: ${book.title}")
+// Subclass of shoe 2
+class FormalShoe(
+    brand: String,
+    size: Int,
+    val info: ShoeInfo
+) : Shoe(brand, size), Wearable {
+    override fun getDetails(): String {
+        return "Formal Shoe - $brand, Size $size, Made of ${info.material}, Color: ${info.color}"
     }
 
-    fun showCatelog() {
-        println("Library Catalog:")
-        books.forEach { println(it.geDescription()) }
+    override fun wear(): String {
+        return "Looking sharp with $brand formal shoes!"
     }
 }
 
