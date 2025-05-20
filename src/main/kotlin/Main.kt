b62ac5c9368e75fdfc6e90ad369e8fc828ec3a2f
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-05-20 22:04:50 +0100

    feat: implemented USA class OOP's

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,78 +1,52 @@
 fun main() {
 
-    val california = USState(
-        populationMillions = 39.24,
-        isCoastal = true,
-        name = "California",
-        capital = "Sacramento",
-        region = Region.WEST
-    )
+}
 
-    val newYork = USState(
-        populationMillions = 19.84,
-        isCoastal = true,
-        name = "New York",
-        capital = "Albany",
-        region = Region.NORTHEAST
-    )
+enum class FuelType {
+    PETROL, DIESEL, ELECTRIC, HYBRID
+}
 
-    println(california.description())
-    println()
-    println(newYork.description())
+data class CarSpecs(
+    val horsepower: Int,
+    val weighKg: Int,
+    val fuelEfficiency: Double
+)
 
-    val branches = listOf(
-        FederalBranch.Execute,
-        FederalBranch.Legislative,
-        FederalBranch.Judicial
-    )
+interface Drivable {
+    fun drive(): String
+}
 
-    branches.forEach {
-        println("${it::class.simpleName}: ${it.dutyDescription()}")
+abstract class Car(
+    val brand:String,
+    val model: String,
+    val fuelType: FuelType,
+    val specs: CarSpecs
+) : Drivable {
+    abstract fun getDescription(): String
+}
+
+class Sedan(
+    brand: String,
+    model: String,
+    fuelType: FuelType,
+    specs: CarSpecs,
+    val luxuryLevel: String
+) : Car(brand, model, fuelType, specs) {
+
+    override fun getDescription(): String {
+        return "$brand $model is a $luxuryLevel sedan with ${specs.horsepower} HP." +
+                "Fuel: ${fuelType.name}, Efficiency: ${specs.fuelEfficiency} km/l or km/kWh"
+    }
+
+    override fun drive(): String {
+        return "Driving $brand $model smoothly on the highway."
     }
 }
 
-enum class Region {
-    NORTHEAST, MIDWEST, SOUTH, WEST
-}
+//class SUV() : Car() {}
 
-interface GovernmentDuty {
-    fun dutyDescription(): String
-}
 
-abstract class State(
-    val name: String,
-    val capital: String,
-    val region: Region
-) {
-    abstract fun description(): String
-}
 
-class USState(
-    val populationMillions: Double,
-    val isCoastal: Boolean,
-     name: String,
-     capital: String,
-     region: Region
-) : State(name, capital, region) {
-    override fun description(): String {
-        val coast = if (isCoastal) "on the coast" else "landlocked"
-        return "$name is a state in the $region. Capital: $capital. Population: $populationMillions million. It is $coast."
-    }
-}
-
-sealed class FederalBranch : GovernmentDuty {
-    object Execute : FederalBranch() {
-        override fun dutyDescription() = "Executes laws, headed by the President."
-    }
-
-    object Legislative : FederalBranch() {
-        override fun dutyDescription() = "Makes laws, consists of Congress."
-    }
-
-    object Judicial : FederalBranch() {
-        override fun dutyDescription() = "Interprets laws, headed by the Supreme Court."
-    }
-}
 
 
 
