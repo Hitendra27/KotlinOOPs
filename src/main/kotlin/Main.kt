bf91bff0f8708897c73869faeb3c446ca2b3aac1
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-05-24 20:15:02 +0100

    feat: implemented Phone class OOP's

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,102 +1,49 @@
 fun main() {
-
-    val tesla = Sedan(
-        brand = "Tesla",
-        model = "Model S",
-        fuelType = FuelType.ELECTRIC,
-        specs = CarSpecs(horsepower = 670, weighKg = 2200, fuelEfficiency = 5.0),
-        luxuryLevel = "Premium"
-    )
-
-    val jeep = SUV(
-        brand = "Jeep",
-        model = "Wrangler",
-        fuelType = FuelType.DIESEL,
-        specs = CarSpecs(horsepower = 285, weighKg = 1960, fuelEfficiency = 10.0),
-        fourWheelDrive = true
-    )
-
-    println(tesla.getDescription())
-    println(tesla.drive())
-
-    println(jeep.getDescription())
-    println(jeep.drive())
-
-    val event: CarEvent = CarEvent.CarStarted(tesla)
-
-    when (event) {
-        is CarEvent.CarStarted -> println("${event.car.model} has started.")
-        is CarEvent.CarStopped -> println("${event.car.model} has stopped.")
-        is CarEvent.CarCrashed -> println("${event.car.model} crashed! Severity: ${event.severity}")
-        CarEvent.NoEvent -> println("No car event occurred")
-    }
-
 }
 
-enum class FuelType {
-    PETROL, DIESEL, ELECTRIC, HYBRID
+interface PhoneFeatures {
+    fun call(number: String)
+    fun text(number: String, message: String)
 }
 
-data class CarSpecs(
-    val horsepower: Int,
-    val weighKg: Int,
-    val fuelEfficiency: Double
+enum class PhoneBrand {
+    SAMSUNG, APPLE, GOOGLE, ONEPLUS
+}
+
+abstract class Phone(
+    val brand: PhoneBrand,
+    val model: String,
+) : PhoneFeatures {
+    abstract fun getOS(): String
+}
+
+data class PhoneOwner(
+    val name: String,
+    val phoneNumber: String
 )
 
-interface Drivable {
-    fun drive(): String
+sealed class PhoneState {
+    object On : PhoneState()
+    object Off : PhoneState()
+    data class AirplaneMode(
+        val enabledTime: String
+    ) : PhoneState()
 }
 
-abstract class Car(
-    val brand:String,
-    val model: String,
-    val fuelType: FuelType,
-    val specs: CarSpecs
-) : Drivable {
-    abstract fun getDescription(): String
-}
-
-class Sedan(
-    brand: String,
+class AndroidPhone(
     model: String,
-    fuelType: FuelType,
-    specs: CarSpecs,
-    val luxuryLevel: String
-) : Car(brand, model, fuelType, specs) {
+    val version: String
+) : Phone(PhoneBrand.GOOGLE, model) {
 
-    override fun getDescription(): String {
-        return "$brand $model is a $luxuryLevel sedan with ${specs.horsepower} HP." +
-                "Fuel: ${fuelType.name}, Efficiency: ${specs.fuelEfficiency} km/l or km/kWh"
+    override fun getOS() = "Android $version"
+
+    override fun call(number: String) {
+        println("Calling $number using Android phone....")
     }
 
-    override fun drive(): String {
-        return "Driving $brand $model smoothly on the highway."
+    override fun text(number: String, message: String) {
+        println("Sending text to $number: $message")
     }
-}
-
-class SUV(
-    brand: String,
-    model: String,
-    fuelType: FuelType,
-    specs: CarSpecs,
-    val fourWheelDrive: Boolean
-) : Car(brand, model, fuelType, specs) {
-
-    override fun getDescription(): String {
-        val driveType = if (fourWheelDrive) "4WD" else "2WD"
-        return "$brand $model is an SUV with $driveType system and ${specs.horsepower} HP."
-    }
-
-    override fun drive(): String {
-        return "Cruising $brand $model off-read."
-    }
-}
-
-sealed class CarEvent {
-    data class CarStarted(val car: Car) : CarEvent()
-    data class CarStopped(val car: Car) : CarEvent()
-    data class CarCrashed(val car: Car, val severity: String): CarEvent()
-    object NoEvent: CarEvent()
 }
 
 
