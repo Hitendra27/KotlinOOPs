cc0bb9599de6d545af5ae99b8015f1af4b58e66a
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2025-05-25 16:19:05 +0100

    feat: implemented Phone class OOP's

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,27 @@
 fun main() {
+
+    val owner = PhoneOwner("Alice", "+123456789")
+    val androidPhone = AndroidPhone("Pixel 7", "14")
+    val ipone = Iphone("iphone 15,", "7")
+
+    val cureentState: PhoneState = PhoneState.AirplaneMode("10.30 AM")
+
+    println("Owner: ${owner.name}, Phone: ${androidPhone.model}")
+    androidPhone.call(owner.phoneNumber)
+    androidPhone.text(owner.phoneNumber, "Hello from Kotlin")
+
+    println("Owner: ${owner.name}, Phone: ${ipone.model}")
+    ipone.call(owner.phoneNumber)
+
+    // Check phone state
+    when (cureentState) {
+        is PhoneState.On -> println("Phone is On")
+        is PhoneState.Off -> println("Phone is off")
+        is PhoneState.AirplaneMode -> println("Phone is in Airplane Mode sice ${cureentState.enabledTime}")
+    }
+
+    println("Android Os: ${androidPhone.getOS()}")
+    println("iPhone OS: ${ipone.getOS()}")
 }
 
 interface PhoneFeatures {
@@ -43,6 +66,24 @@ class AndroidPhone(
 
     override fun text(number: String, message: String) {
         println("Sending text to $number: $message")
+    }
+}
+
+class Iphone(
+    model: String,
+    val iosVersion: String
+) : Phone(PhoneBrand.APPLE, model) {
+
+    override fun getOS(): String {
+         return "iOS $iosVersion"
+    }
+
+    override fun call(number: String) {
+        println("Calling $number using Android phone...")
+    }
+
+    override fun text(number: String, message: String) {
+       println("Sending text to $number: $message")
     }
 }
 
